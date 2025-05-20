8ec59c83eddcab0f442f0b6fd1e1e5f6f8deec0d
Son-Gyeongi <thsruddl77@gmail.com>
2025-05-20 10:42:06 +0900

    chapter11 Stack

---
+++ src/chapter11/stack/Stack3.java
@@ -0,0 +1,32 @@
+package chapter11.stack;
+
+import java.util.Stack;
+
+/*
+페이지 374
+ */
+public class Stack3 {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        stack.add(4);
+        stack.add(5);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        System.out.println(stack.isEmpty());
+
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+
+        System.out.println(stack.isEmpty());
+    }
+}
